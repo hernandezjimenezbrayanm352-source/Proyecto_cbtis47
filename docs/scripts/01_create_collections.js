ac54189b71b09c7195ce22a3086c89b5f73bde5a
argel183 <rueda.jaime.maria.argel.m3s2@gmail.com>
2026-02-24 13:41:38 -0600

    Actualizar 01_create_collections.js

--- docs/scripts/01_create_collections.js
+++ docs/scripts/01_create_collections.js
@@ -1,1 +1,11 @@
-.
+// Seleccionar base de datos
+use survey_db
+
+// Crear colecciones principales
+db.createCollection("surveys")
+db.createCollection("questions")
+db.createCollection("options")
+db.createCollection("responses")
+db.createCollection("answers")
+
+print("Collections for Survey System created successfully!")
