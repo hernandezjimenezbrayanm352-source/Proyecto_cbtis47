4e9fefab0933d4a9c417547f8c856e8d2cc63094
martinezrendonarizbethm352-source <martinez.rendon.arizbeth.m352@gmail.com>
2026-03-09 08:27:49 -0600

    Actualizar seeds.js

--- docs/data/seeds.js
+++ docs/data/seeds.js
@@ -34,3 +34,40 @@ db.usuarios.insertMany([
   { usuario_id: "U019", nombre: "Usuario 19", correo: "usuario19@gmail.com", edad: 16 },
   { usuario_id: "U020", nombre: "Usuario 20", correo: "usuario20@gmail.com", edad: 17 }
 ])
+// =============================
+// Insertar otros 30 Usuarios
+// =============================
+db.usuarios.insertMany([
+  { usuario_id: "U021", nombre: "Usuario 21", correo: "usuario21@gmail.com", edad: 16 },
+  { usuario_id: "U022", nombre: "Usuario 22", correo: "usuario22@gmail.com", edad: 17 },
+  { usuario_id: "U023", nombre: "Usuario 23", correo: "usuario23@gmail.com", edad: 18 },
+  { usuario_id: "U024", nombre: "Usuario 24", correo: "usuario24@gmail.com", edad: 16 },
+  { usuario_id: "U025", nombre: "Usuario 25", correo: "usuario25@gmail.com", edad: 17 },
+  { usuario_id: "U026", nombre: "Usuario 26", correo: "usuario26@gmail.com", edad: 18 },
+  { usuario_id: "U027", nombre: "Usuario 27", correo: "usuario27@gmail.com", edad: 16 },
+  { usuario_id: "U028", nombre: "Usuario 28", correo: "usuario28@gmail.com", edad: 17 },
+  { usuario_id: "U029", nombre: "Usuario 29", correo: "usuario29@gmail.com", edad: 18 },
+  { usuario_id: "U030", nombre: "Usuario 30", correo: "usuario30@gmail.com", edad: 16 },
+
+  { usuario_id: "U031", nombre: "Usuario 31", correo: "usuario31@gmail.com", edad: 17 },
+  { usuario_id: "U032", nombre: "Usuario 32", correo: "usuario32@gmail.com", edad: 18 },
+  { usuario_id: "U033", nombre: "Usuario 33", correo: "usuario33@gmail.com", edad: 16 },
+  { usuario_id: "U034", nombre: "Usuario 34", correo: "usuario34@gmail.com", edad: 17 },
+  { usuario_id: "U035", nombre: "Usuario 35", correo: "usuario35@gmail.com", edad: 18 },
+  { usuario_id: "U036", nombre: "Usuario 36", correo: "usuario36@gmail.com", edad: 16 },
+  { usuario_id: "U037", nombre: "Usuario 37", correo: "usuario37@gmail.com", edad: 17 },
+  { usuario_id: "U038", nombre: "Usuario 38", correo: "usuario38@gmail.com", edad: 18 },
+  { usuario_id: "U039", nombre: "Usuario 39", correo: "usuario39@gmail.com", edad: 16 },
+  { usuario_id: "U040", nombre: "Usuario 40", correo: "usuario40@gmail.com", edad: 17 },
+
+  { usuario_id: "U041", nombre: "Usuario 41", correo: "usuario41@gmail.com", edad: 18 },
+  { usuario_id: "U042", nombre: "Usuario 42", correo: "usuario42@gmail.com", edad: 16 },
+  { usuario_id: "U043", nombre: "Usuario 43", correo: "usuario43@gmail.com", edad: 17 },
+  { usuario_id: "U044", nombre: "Usuario 44", correo: "usuario44@gmail.com", edad: 18 },
+  { usuario_id: "U045", nombre: "Usuario 45", correo: "usuario45@gmail.com", edad: 16 },
+  { usuario_id: "U046", nombre: "Usuario 46", correo: "usuario46@gmail.com", edad: 17 },
+  { usuario_id: "U047", nombre: "Usuario 47", correo: "usuario47@gmail.com", edad: 18 },
+  { usuario_id: "U048", nombre: "Usuario 48", correo: "usuario48@gmail.com", edad: 16 },
+  { usuario_id: "U049", nombre: "Usuario 49", correo: "usuario49@gmail.com", edad: 17 },
+  { usuario_id: "U050", nombre: "Usuario 50", correo: "usuario50@gmail.com", edad: 18 }
+])
